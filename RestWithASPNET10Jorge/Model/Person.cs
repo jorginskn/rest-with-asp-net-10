6b4f9e4c384bb689ee788762ac1ffefba623820f
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2026-04-08 12:07:58 -0300

    Adiciona Data Annotations à entidade Person
    Inclui anotações do Entity Framework na classe Person para mapear propriedades às colunas da tabela "person", definir restrições de tamanho, obrigatoriedade e mensagens de erro em português. Adiciona também os namespaces necessários para suportar essas anotações.

--- RestWithASPNET10Jorge/Model/Person.cs
+++ RestWithASPNET10Jorge/Model/Person.cs
@@ -1,11 +1,34 @@
-﻿namespace RestWithASPNET10Jorge.Model;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace RestWithASPNET10Jorge.Model;
+
+[Table("person")]
 public class Person
 {
+    [Key]
+    [Column("id")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get;  set; }
+
+    [Required(ErrorMessage = "Primeiro nome é obrigatório")]
+    [Column("first_name", TypeName = "varchar(80)")]
+    [MaxLength(80, ErrorMessage = "O primeiro nome deve conter no máximo 80 caracteres")]
     public string FirstName { get;  set; }
+
+    [Required(ErrorMessage = "Sobrenome é obrigatório")]
+    [Column("last_name", TypeName = "varchar(80)")]
+    [MaxLength(80, ErrorMessage = "O sobrenome deve conter no máximo 80 caracteres")]
     public string LastName { get;   set; }
+
+    [Required(ErrorMessage = "Gênero é obrigatório")]
+    [Column("gender", TypeName = "varchar(6)")]
+    [MaxLength(6, ErrorMessage = "O gênero deve conter no máximo 6 caracteres")]
     public string Gender     { get;   set; }
+
+    [Required(ErrorMessage = "Endereço é obrigatório")]
+    [Column("address", TypeName = "varchar(100)")]
+    [MaxLength(100, ErrorMessage = "O endereço deve conter no máximo 100 caracteres")]
     public string Address { get;   set; }
 
 }
